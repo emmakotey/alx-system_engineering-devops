1f8846777a7f79b67d3a2f045ac38a54e3eea094
emmakotey <emmakotey@gmail.com>
2023-05-09 18:10:01 +0000

    setup

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+exec {'config file':
+path     => ['/usr/bin', '/bin'],
+command  => "sudo sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
+provider => 'shell',
+}
